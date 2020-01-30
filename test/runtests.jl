8e0cb69bfdbda059c26b5b0ee5a968b85a08a3cd
jondea <jondea@thedeakin.net>
2020-01-30 13:20:12 +0000

    Simplify syntax of testing arrays are approx the same

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,12 +13,12 @@ include("utils.jl")
         small_values = 1e-4 .*[1+0im, 1im, -1+0im, -1im]
 
         @testset "H ≈ H_asymptotic" begin
-            @test all(isapprox.(small_arg_hankelh1.(0, small_values), hankelh1.(0, small_values); rtol=0.1))
-            @test all(isapprox.(small_arg_hankelh1.(1, small_values), hankelh1.(1, small_values); rtol=1e-5))
-            @test all(isapprox.(small_arg_hankelh1.(2, small_values), hankelh1.(2, small_values); rtol=1e-5))
-            @test all(isapprox.(small_arg_hankelh1.(4, small_values), hankelh1.(4, small_values); rtol=1e-5))
-            @test all(isapprox.(small_arg_hankelh1.(8, small_values), hankelh1.(8, small_values); rtol=1e-5))
-            @test all(isapprox.(small_arg_hankelh1.(11, small_values), hankelh1.(11, small_values); rtol=1e-5))
+            @test small_arg_hankelh1.(0, small_values)  ≈ hankelh1.(0, small_values)  rtol=0.1
+            @test small_arg_hankelh1.(1, small_values)  ≈ hankelh1.(1, small_values)  rtol=1e-5
+            @test small_arg_hankelh1.(2, small_values)  ≈ hankelh1.(2, small_values)  rtol=1e-5
+            @test small_arg_hankelh1.(4, small_values)  ≈ hankelh1.(4, small_values)  rtol=1e-5
+            @test small_arg_hankelh1.(8, small_values)  ≈ hankelh1.(8, small_values)  rtol=1e-5
+            @test small_arg_hankelh1.(11, small_values) ≈ hankelh1.(11, small_values) rtol=1e-5
         end
 
         # Test whether f f⁻¹ = identity for different Hankel order (n) and solution branches (b)
@@ -33,7 +33,7 @@ include("utils.jl")
                     if !all(isapprox.((f  ∘ invf).(testvec), testvec; rtol=1e-5))
                         @show n, b, abs.(((f  ∘ invf).(testvec) .- testvec)./testvec)
                     end
-                    @test all(isapprox.((f  ∘ invf).(testvec), testvec; rtol=1e-5))
+                    @test (f  ∘ invf).(testvec) ≈ testvec rtol=1e-5
                 end
             end
         end
@@ -46,12 +46,12 @@ include("utils.jl")
         large_values = [1.0e8+0im, 1.0e8+0.5im, 1.0e8-0.5im]
 
         @testset "H ≈ H_asymptotic" begin
-            @test all(isapprox.(large_arg_hankelh1.(0, large_values), hankelh1.(0, large_values); rtol=1e-4))
-            @test all(isapprox.(large_arg_hankelh1.(1, large_values), hankelh1.(1, large_values); rtol=1e-4))
-            @test all(isapprox.(large_arg_hankelh1.(2, large_values), hankelh1.(2, large_values); rtol=1e-4))
-            @test all(isapprox.(large_arg_hankelh1.(3, large_values), hankelh1.(3, large_values); rtol=1e-4))
-            @test all(isapprox.(large_arg_hankelh1.(4, large_values), hankelh1.(4, large_values); rtol=1e-4))
-            @test all(isapprox.(large_arg_hankelh1.(8, large_values), hankelh1.(8, large_values); rtol=1e-4))
+            @test large_arg_hankelh1.(0, large_values) ≈ hankelh1.(0, large_values) rtol=1e-4
+            @test large_arg_hankelh1.(1, large_values) ≈ hankelh1.(1, large_values) rtol=1e-4
+            @test large_arg_hankelh1.(2, large_values) ≈ hankelh1.(2, large_values) rtol=1e-4
+            @test large_arg_hankelh1.(3, large_values) ≈ hankelh1.(3, large_values) rtol=1e-4
+            @test large_arg_hankelh1.(4, large_values) ≈ hankelh1.(4, large_values) rtol=1e-4
+            @test large_arg_hankelh1.(8, large_values) ≈ hankelh1.(8, large_values) rtol=1e-4
         end
 
         # Test whether f f⁻¹ = identity for different Hankel order (n) and solution branches (b)
@@ -67,7 +67,7 @@ include("utils.jl")
                     if !all(isapprox.((f  ∘ invf).(testvec), testvec; rtol=1e-5))
                         @show n, b, testvec, invf.(testvec), (f  ∘ invf).(testvec)
                     end
-                    @test all(isapprox.((f  ∘ invf).(testvec), testvec; rtol=1e-5))
+                    @test (f  ∘ invf).(testvec) ≈ testvec rtol=1e-5
                 end
             end
         end
